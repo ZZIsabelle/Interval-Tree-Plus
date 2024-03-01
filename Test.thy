6b08bbad5a5c14f6e53411d501debe83f188e38a
ZZIsabelle <157561423+ZZIsabelle@users.noreply.github.com>
2024-03-01 21:14:08 +0800

    Update Test.thy

--- Test.thy
+++ Test.thy
@@ -9,10 +9,10 @@ definition nat_ivl :: "nat ivl" where
 definition nat_list::"(nat ivl) list"where
 "nat_list  =  [Abs_ivl (0, 5),Abs_ivl (4, 5)]"
 lemma"insert nat_ivl ivl_tree = ivl_tree"
-  apply(simp add:insert_def nat_ivl_def ivl_tree_def max3_def high_def)
+  apply(simp add:insert_IntervalT_def nat_ivl_def ivl_tree_def max3_def high_def)
   by(simp add: Abs_ivl_inverse Rep_ivl_inverse)
 lemma"insert (Abs_ivl (0::nat, 5::nat)) Leaf = ivl_tree"
-  apply(simp add:insert_def max3_def high_def ivl_tree_def)
+  apply(simp add:insert_IntervalT_def max3_def high_def ivl_tree_def)
   by(simp add: Abs_ivl_inverse Rep_ivl_inverse)
 
 definition CS::"nat Coordinate_System" where
@@ -27,22 +27,22 @@ lemma test2:"Y_Dimension CS = [Abs_ivl (10::nat,22::nat),Abs_ivl (14,20)]"
 
 lemma test3:"(X_P_Achievements CS \<langle>\<rangle>) = 
 \<langle>\<langle>\<rangle>,((Abs_ivl (5::nat,12::nat),28::nat),Black),\<langle>\<langle>\<rangle>,((Abs_ivl (16::nat,28::nat),28::nat),Red),\<langle>\<rangle>\<rangle>\<rangle>"
-  apply(simp add:CS_def X_P_Achievements_def X_Dimension_def map_Publication_def insert_def max3_def high_def)
+  apply(simp add:CS_def X_P_Achievements_def X_Dimension_def map_Publication_def insert_IntervalT_def max3_def high_def)
   by(simp add: Abs_ivl_inverse ivl_less low_def high_def)
 
 lemma test4:"(Y_P_Achievements CS \<langle>\<rangle>) = 
 \<langle>\<langle>\<rangle>,((Abs_ivl (10::nat,22::nat),22::nat),Black),\<langle>\<langle>\<rangle>,((Abs_ivl (14::nat,20::nat),20::nat),Red),\<langle>\<rangle>\<rangle>\<rangle>"
-  apply(simp add:CS_def Y_P_Achievements_def Y_Dimension_def map_Publication_def insert_def max3_def)
+  apply(simp add:CS_def Y_P_Achievements_def Y_Dimension_def map_Publication_def insert_IntervalT_def max3_def)
   by(simp add: Abs_ivl_inverse ivl_less low_def high_def)
 
 
 lemma test5:"Match CS Leaf (Abs_ivl (14::nat,21::nat),Abs_ivl (6::nat,16::nat)) = True"
-  apply(simp add:Match_def judge_def CS_def Y_P_Achievements_def Y_Dimension_def map_Publication_def insert_def X_P_Achievements_def X_Dimension_def)
+  apply(simp add:Match_def judge_def CS_def Y_P_Achievements_def Y_Dimension_def map_Publication_def insert_IntervalT_def X_P_Achievements_def X_Dimension_def)
   apply(simp add:overlap_def low_def high_def Abs_ivl_inverse ivl_less )
   by auto
 
 lemma test6:"Match CS Leaf (Abs_ivl (26::nat,34::nat),Abs_ivl (4::nat,12::nat)) = False "
-  apply(simp add:Match_def judge_def CS_def Y_P_Achievements_def Y_Dimension_def map_Publication_def insert_def X_P_Achievements_def X_Dimension_def)
+  apply(simp add:Match_def judge_def CS_def Y_P_Achievements_def Y_Dimension_def map_Publication_def insert_IntervalT_def X_P_Achievements_def X_Dimension_def)
   by(simp add:overlap_def low_def high_def Abs_ivl_inverse ivl_less max3_def)
  
 
